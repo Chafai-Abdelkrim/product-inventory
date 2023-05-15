0d0470e7ae8265319c94eccccf2b599c90e354e6
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-05-15 11:44:27 +0100

    added protect middleware function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -119,7 +119,22 @@ const logout = asyncHandler(async (req, res) => {
 });
 
 const getUser = asyncHandler(async (req, res) => {
-  res.send("get user data");
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    const { _id, name, email, photo, phone, bio } = user;
+    res.status(200).json({
+      _id,
+      name,
+      email,
+      photo,
+      phone,
+      bio,
+    });
+  } else {
+    res.status(400);
+    throw new Error("User Not Found");
+  }
 });
 
 module.exports = {
