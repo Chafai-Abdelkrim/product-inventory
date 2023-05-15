95d9972ba29efbbccd76f7ed8b70a520e7559a13
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-05-15 12:06:47 +0100

    created login status function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -138,7 +138,14 @@ const getUser = asyncHandler(async (req, res) => {
 });
 
 const loginStatus = asyncHandler(async (req, res) => {
-  res.send("login status")
+  const token = req.cookies.token;
+  if (!token) return res.json(false);
+
+  //Verify Token
+  const verified = jwt.verify(token, process.env.JWT_SECRET);
+  if (verified) return res.json(true);
+
+  return res.json(false);
 });
 
 module.exports = {
