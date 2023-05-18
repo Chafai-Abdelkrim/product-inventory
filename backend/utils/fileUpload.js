9372d3ad9bc5903ea8818bbf039b4d813e82124c
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-05-18 10:27:10 +0100

    created multer fileUpload

---
+++ backend/utils/fileUpload.js
@@ -0,0 +1,42 @@
+const multer = require("multer");
+
+//define file storage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      new Date().toISOString().replace(/:/g, "-") + "-" + file.originalname
+    );
+  },
+});
+
+//Specify file formate that can be saved
+function fileFilter(req, file, cb) {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+}
+
+const upload = multer({ storage, fileFilter });
+
+//File Size Formatter
+const fileSizeFormatter = (bytes, decimal) => {
+  if (bytes === 0) {
+    return "0 Bytes";
+  }
+  const dm = decimal || 2;
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "YB", "ZB"];
+  const i = Math.floor(Math.log(bytes) / Math.log(1024));
+  return parseFloat((bytes / Math.pow(1024, i)).toFixed(dm)) + " " + sizes[i];
+};
+
+module.exports = { upload, fileSizeFormatter };
