8acfe879aa06acb176ea280ed11f46916ea04dba
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-05-22 11:13:51 +0100

    created redux authSlice

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,18 @@ import {useEffect} from 'react';
 import { useDispatch } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import { SET_LOGIN } from './redux/features/auth/authSlice';
 
 function App() {
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    async function loginStatus() {
+      const status = await getLoginStatus();
+      dispatch(SET_LOGIN(status))
+    }
+    loginStatus();
+  }, [dispatch]);
 
   return (
     <BrowserRouter>
