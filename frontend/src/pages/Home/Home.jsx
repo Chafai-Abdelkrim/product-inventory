47f5b9d0ba3f27971df8c7f6b8e36b72e6730fb3
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-05-22 12:04:20 +0100

    created Home page

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,74 @@
+import { RiProductHuntLine } from 'react-icons/ri';
+import { Link } from 'react-router-dom';
+import './Home.scss';
+import heroImg from '../../assets/inv-img.png';
+import { ShowOnLogout, ShowOnLogin } from '../../components/protect/HiddenLink';
+
+const NumberText = ({ num, text }) => {
+  return (
+    <div className="--mr">
+      <h3 className="--color-white">{num}</h3>
+      <p className="--color-white">{text}</p>
+    </div>
+  );
+};
+
+const Home = () => {
+  return (
+    <div className="home">
+      <nav className="container --flex-between">
+        <div className="logo">
+          <RiProductHuntLine size={35} />
+        </div>
+
+        <ul className="home-links">
+          <ShowOnLogout>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </ShowOnLogout>
+          <ShowOnLogout>
+            <li>
+              <button className="--btn --btn-primary">
+                <Link to="/login">Login</Link>
+              </button>
+            </li>
+          </ShowOnLogout>
+          <ShowOnLogin>
+            <li>
+              <button className="--btn --btn-primary">
+                <Link to="/dashboard">Dashboard</Link>
+              </button>
+            </li>
+          </ShowOnLogin>
+        </ul>
+      </nav>
+
+      <section className="container hero">
+        <div className="hero-text">
+          <h2>Inventory {'&'} Stock Management Solution</h2>
+          <p>
+            Inventory system to control and manage proucts in the warehouse in
+            real timeand integrated to make it easier to develop your business.
+          </p>
+          <div className="hero-buttons">
+            <button className="--btn --btn-secondary">
+              <Link to="/dashboard">Free Trial for 1 month</Link>
+            </button>
+          </div>
+          <div className="--flex-start">
+            <NumberText num="6k" text="Brand Owners" />
+            <NumberText num="18k" text="Active Users" />
+            <NumberText num="100+" text="Partners" />
+          </div>
+        </div>
+
+        <div className="hero-image">
+          <img src={heroImg} alt="Inventory app" />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
