07557b52f3ea48a4756e64cff236541d33b16bac
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-05-23 09:09:47 +0100

    Created auth pages and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,10 @@ import { useDispatch } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import Home from './pages/Home/Home';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Forgot from './pages/auth/Forgot';
+import Reset from './pages/auth/Reset';
 import { SET_LOGIN } from './redux/features/auth/authSlice';
 
 function App() {
@@ -21,6 +25,10 @@ function App() {
     <ToastContainer />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/forgot' element={<Forgot />} />
+        <Route path='/resetpassword/:resetToken' element={<Reset />} />
       </Routes>
     </BrowserRouter>
   );
