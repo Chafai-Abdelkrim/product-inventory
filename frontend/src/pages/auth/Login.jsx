16644845d8d6f7a140598bcdcc8a8ef765b60413
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-05-23 10:30:41 +0100

    added Login page

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,14 +1,100 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { SET_LOGIN, SET_NAME } from '../../redux/features/auth/authSlice';
+import { loginUser, validateEmail } from '../../services/authService';
+import Loader from '../../components/loader/Loader';
+import Card from '../../components/Card/Card';
+import { BiLogIn } from 'react-icons/bi';
 import styles from './auth.module.scss';
 
 const initialState = {
-    email: "",
-    password: "",
-}
+  email: '',
+  password: '',
+};
 
 const Login = () => {
-    return (
-        <div></div>
-    )
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [formData, setFormData] = useState(initialState);
+  const { email, password } = formData;
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const login = async (e) => {
+    e.preventDefault();
+
+    if (!email || !password) {
+      return toast.error('All fields are required');
+    }
+    if (!validateEmail(email)) {
+      return toast.error('Please enter a valid email');
+    }
+
+    const userData = {
+      email,
+      password,
+    };
+    setIsLoading(true);
+    try {
+      const data = await loginUser(userData);
+      console.log(data);
+      await dispatch(SET_LOGIN(true));
+      await dispatch(SET_NAME(data.name));
+      navigate('/dashboard');
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className={`container ${styles.auth}`}>
+      {isLoading && <Loader />}
+      <Card>
+        <div className={styles.form}>
+          <div className="--flex-center">
+            <BiLogIn size={35} color="#999" />
+          </div>
+          <h2>Login</h2>
+
+          <form onSubmit={login}>
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              value={email}
+              onChange={handleInputChange}
+              required
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={password}
+              onChange={handleInputChange}
+              required
+            />
+            <button className="--btn --btn-primary --btn-block" type="submit">
+              Login
+            </button>
+          </form>
+          <Link to="/forgot">Forgot Password</Link>
+
+          <span className={styles.register}>
+            <Link to="/">Home</Link>
+            <p> &nbsp; Don't have an account? &nbsp;</p>
+            <Link to="/register">Register</Link>
+          </span>
+        </div>
+      </Card>
+    </div>
+  );
 };
 
 export default Login;
