8bbcaba77fb705d7a7797e30c43617bf61f6d134
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-05-29 16:01:19 +0100

    finished Dashboard page

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,17 +1,32 @@
-import { useDispatch, useSelector } from "react-redux";
-import { selectIsLoggedIn } from "../../redux/features/auth/authSlice";
-import useRedirectLoggedOutUser from "../../customHook/useRedirectLoggedOutUser";
-
+import { useDispatch, useSelector } from 'react-redux';
+import { selectIsLoggedIn } from '../../redux/features/auth/authSlice';
+import useRedirectLoggedOutUser from '../../customHook/useRedirectLoggedOutUser';
+import getProducts from '../../redux/features/product/productSlice';
+import ProductSummary from '../../components/product/productSummary/ProductSummary';
+import ProductList from '../../components/product/productList/ProductList';
+import { useEffect } from 'react';
 
 const Dashboard = () => {
-    useRedirectLoggedOutUser("/login");
-    const dispatch = useDispatch();
+  useRedirectLoggedOutUser('/login');
+  const dispatch = useDispatch();
 
-    const isLoggedIn = useSelector(selectIsLoggedIn);
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const { products, isLoading, isError, message } = useSelector(
+    (state) => state.product
+  );
 
-    return (
-        <div></div>
-    )
-}
+  useEffect(() => {
+    if (isLoggedIn === true) dispatch(getProducts());
+
+    if (isError) console.log(message);
+  }, [isLoading, isError, message, dispatch]);
+
+  return (
+    <div>
+      <ProductSummary products={products} />
+      <ProductList products={products} isLoading={isLoading} />
+    </div>
+  );
+};
 
 export default Dashboard;
