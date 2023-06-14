5b09e3a51ca1733e589a721760e2df7de80703da
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-06-14 11:23:52 +0100

    changed secure to true

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -44,7 +44,7 @@ const registerUser = asyncHandler(async (req, res) => {
     httpOnly: true,
     expires: new Date(Date.now() + 1000 * 86400), // 1 day
     sameSite: "none",
-    secure: false,
+    secure: true,
   });
 
   if (user) {
@@ -92,7 +92,7 @@ const loginUser = asyncHandler(async (req, res) => {
       httpOnly: true,
       expires: new Date(Date.now() + 1000 * 86400), // 1 day
       sameSite: "none",
-      secure: false,
+      secure: true,
     });
   }
 
@@ -119,7 +119,7 @@ const logout = asyncHandler(async (req, res) => {
     httpOnly: true,
     expires: new Date(0),
     sameSite: "none",
-    secure: false,
+    secure: true,
   });
   return res.status(200).json({ message: "Successfully Logged Out" });
 });
